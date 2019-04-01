250beab794817a10bc3ccabacae2b977cbe5bf39
schoudhari <schoudhari@techracers.io>
2019-04-02 02:07:48 +0530

    Added profile and update functionality using collapse

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
     });
   });
 
+  var uuId,uuName,uuEmail,uuPass;
   $("#viewUser").click(function () {
     $.ajax({
       type: 'get',
@@ -20,16 +21,58 @@ $(document).ready(function () {
       dataType: 'json',
       contentType: "application/json",
       success: function (data) {
-        console.log(data);
-        window.location = "/app/profile";
+        $("#u_id").text(data.id);
+        $("#u_name").text(data.name);
+        $("#u_email").text(data.email);
+        $("#u_password").text(data.password);
       }
+    }); 
+  });
+
+  $("#viewUser1").click(function () {
+    $.ajax({
+      type: 'get',
+      url: '/app/profile',
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        uuId = data.id;
+        uuName = data.name;
+        uuEmail = data.email;
+        uuPass = data.Pass;
+        $("#uu_id").text(data.id);
+        $("#uu_name").text(data.name);
+        $("#uu_email").text(data.email);
+        $("#uu_password").text(data.password);
+      }
+    }); 
+  });
+
+  $("#updateUser").click(function () {
+    // alert("=============");
+    console.log("==================");
+    var id = $("#uu_id").text();
+    console.log(id);
+    var data = {
+      name: $("#uu_name").text(),
+      email: $("#uu_email").text(),
+      password: $("#uu_password").text()
+    }
+    console.log(data);
+    $.ajax({
+      type: 'PUT',
+      url: '/app/users/'+id,
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+    }).done(function () {
+      alert("Profile Updated!");
     });
   });
 
   $("#deleteUser").click(function () {
     var id = $("#userId1").val();
     console.log(id);
-    var link = '/app/users/'+id;
+    var link = '/app/users/' + id;
     $.ajax({
       type: 'delete',
       url: link,
@@ -83,7 +126,7 @@ $(document).ready(function () {
 
   $("#submitUser").click(function () {
     var id = $("#userId").val();
-    var link = '/app/specificUser/'+id;
+    var link = '/app/users/' + id;
     $.ajax({
       type: 'get',
       url: link,
@@ -108,4 +151,140 @@ $(document).ready(function () {
       }
     });
   });
+
+  $("#allTrainees").click(function () {
+    console.log("==========");
+    $.ajax({
+      type: 'get',
+      url: '/app/users/trainees',
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        window.location = "/app/users/trainees";
+      }
+    });
+  });
+
+  $("#submitModule").click(function () {
+    var id = $("#moduleId").val();
+    var link = '/app/modules/' + id;
+    $.ajax({
+      type: 'get',
+      url: link,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        window.location = link;
+      }
+    });
+  });
+
+  $("#updateModule").click(function () {
+    var id = $("#moduleId1").val();
+    var link = '/app/modules/' + id;
+    $.ajax({
+      type: 'put',
+      url: link,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        window.location = link;
+      }
+    });
+  });
+
+  $("#deleteModule").click(function () {
+    var id = $("#moduleId2").val();
+    console.log(id);
+    var link = '/app/modules/' + id;
+    $.ajax({
+      type: 'delete',
+      url: link,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        window.location = '/trainerDash';
+      }
+    });
+  });
+
+  $("#getAllTaskModule").click(function () {
+    var id = $("#moduleId3").val();
+    var link = '/app/modules/' + id + '/tasks';
+    $.ajax({
+      type: 'get',
+      url: link,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        window.location = link;
+      }
+    });
+  });
+
+  $("#specificTask").click(function () {
+    var id = $("#taskId").val();
+    var link = '/app/tasks/' + id;
+    $.ajax({
+      type: 'get',
+      url: link,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        window.location = link;
+      }
+    });
+  });
+
+  $("#deleteTask").click(function () {
+    var id = $("#taskId1").val();
+    var link = '/app/tasks/' + id;
+    $.ajax({
+      type: 'delete',
+      url: link,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        window.location = link;
+      }
+    });
+  });
+
+  $("#specificLog").click(function () {
+    var id = $("#logId").val();
+    var link = '/app/logs/' + id;
+    $.ajax({
+      type: 'get',
+      url: link,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        window.location = link;
+      }
+    });
+  });
+
+  $("#deleteLog").click(function () {
+    var id = $("#logId1").val();
+    var link = '/app/logs/' + id;
+    $.ajax({
+      type: 'delete',
+      url: link,
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        window.location = link;
+      }
+    });
+  });
+
 })
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ var Task = require('../controller/task')
 var Log = require('../controller/log')
 var Status = require('../controller/status')
 var role = require('../controller/auth');
-var mail = require('../utils/email');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
   res.redirect('/signin');
 });
@@ -16,8 +14,9 @@ router.get('/', function (req, res, next) {
 //users
 router.get('/app/users', User.getUsers);
 router.get('/app/profile', User.profile);
-router.get('/app/specificUser/:id', User.specificUser);
+router.get('/app/users/:id', User.specificUser);
 router.put('/app/users/:id', role.traineeTrainerRole, User.updateUser);
+router.get('/app/users/trainees', role.trainerRole, User.getTrainees);
 router.delete('/app/users/:id', role.adminRole, User.deleteUser);
 router.put('/app/users/:id/updateTrainer', role.adminRole, User.updateTrainer);
 router.post('/app/users/:id/taskDone', role.traineeRole, User.taskDone);
@@ -25,7 +24,7 @@ router.post('/app/users/:id/assignModule', role.trainerRole, User.assignModule);
 
 //modules
 router.get('/app/modules', role.redirectLogin, Module.getModules);
-router.post('/app/modules', role.trainerRole,Module.newModule);
+router.post('/app/modules', role.trainerRole,Module.newModule);  //
 router.get('/app/modules/:id', role.traineeTrainerRole, Module.specificModule);
 router.put('/app/modules/:id', role.trainerRole, Module.updateModule);
 router.delete('/app/modules/:id', role.trainerRole, Module.deleteModule);
@@ -59,7 +58,6 @@ router.get('/signin', role.redirectHome, (req, res) => {
 }); 
 
 router.get('/home', role.redirectLogin, (req, res) => {
-  console.log(req.session);
   res.render('home');
 });
 
